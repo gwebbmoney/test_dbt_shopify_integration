ba2578a745c5caeff5d9d87bcf1d663c8b3b8864
Garand Webb <gwebb@red-aspen.com>
2023-10-20 17:40:45 +0000

    per month sync

--- models/data_marts/redaspen_per_day_sales_tax_and_shipping_revenues.sql
+++ models/data_marts/redaspen_per_day_sales_tax_and_shipping_revenues.sql
@@ -1,4 +1,4 @@
-WITH sales_tax_and_shipping_revenue AS(
+WITH per_day_sales_tax_and_shipping_revenue AS(
     SELECT created_at::date AS day,
         SUM(sales_tax_amount_cents - sales_tax_refund_cents) AS sales_tax_cents,
         SUM(shipping_amount_cents - shipping_refund_cents) AS shipping_amount_cents
@@ -6,4 +6,4 @@ WITH sales_tax_and_shipping_revenue AS(
     GROUP BY day
 )
 SELECT *
-FROM sales_tax_and_shipping_revenue
+FROM per_day_sales_tax_and_shipping_revenue
