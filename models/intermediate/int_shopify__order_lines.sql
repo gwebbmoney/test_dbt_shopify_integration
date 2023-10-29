5b66396e288c2f8cfc938d6d1da09b99e45eb91d
Garand Webb <gwebb@red-aspen.com>
2023-10-29 00:24:50 +0000

    FILE CHANGE

--- models/intermediate/int_shopify__order_lines.sql
+++ models/intermediate/int_shopify__order_lines.sql
@@ -1,5 +1,20 @@
 WITH order_lines AS(
-    SELECT * FROM {{ source("shopify_raw", 'ORDER_LINE') }}
+    SELECT * FROM {{ source('shopify_raw', 'ORDER_LINE') }}
 )
-SELECT *
+SELECT id AS order_line_id,
+    order_id,
+    product_id,
+    variant_id, 
+    title AS product_name,
+    variant_title AS product_variant_name,
+    sku,
+    index AS order_line,
+    price*100 AS price_cents,
+    quantity AS quantity_ordered,
+    fulfillable_quantity,
+    (price_cents * quantity) AS line_item_price_cents,
+    properties,
+    total_discount*100 AS total_discount_cents,
+    pre_tax_price*100 AS pre_tax_price_cents,
+    gift_card
 FROM order_lines
