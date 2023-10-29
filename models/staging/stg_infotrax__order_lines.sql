99aeef49a34ce686bb4f2b5d8c583e80c549b704
Garand Webb <gwebb@red-aspen.com>
2023-10-29 00:20:31 +0000

    product id

--- models/staging/stg_infotrax__order_lines.sql
+++ models/staging/stg_infotrax__order_lines.sql
@@ -3,11 +3,29 @@ WITH raw_order_lines AS(
 ),
 raw_infotrax_orders AS(
     SELECT * FROM {{ ref("stg_infotrax__orders")}}
+),
+product_bundle_base AS(
+    SELECT s.name AS sku,
+        p.name as product_name, 
+        p.id as product_id,
+        s.id as sku_id
+    FROM {{ source("redaspen", 'PRODUCTS') }} p
+        JOIN {{ source("redaspen", 'SKUS') }} s ON p.id = s.skuable_id
+    WHERE s.skuable_type = 'Product'
+    UNION
+    SELECT s.name as sku,
+        b.name,
+        b.id,
+        s.id
+    FROM {{ source("redaspen", 'BUNDLES') }} b
+        JOIN {{ source("redaspen", 'SKUS') }} s ON b.id = s.skuable_id
+    WHERE s.skuable_type = 'Bundle'
 )
 SELECT ol.id,
     ol.sales_price * 100 as sales_price_cents,
+    pbb.product_id,
     ol.order_number as infotrax_order_number,
-    ol.item_name_1 as product_name,
+    COALESCE(pbb.product_name, ol.item_name_1) AS product_name,
     ol.quantity_ordered,
     ol.quantity_returned,
     ol.order_line,
@@ -22,10 +40,12 @@ SELECT ol.id,
     ol.item_code as infotrax_sku,
     ol.flag_2 as component_status,
     rio.currency_code,
+    pbb.sku_id,
     ol.promo_id,
     ol.kit_line,
     ol._FIVETRAN_DELETED
 FROM raw_order_lines ol LEFT JOIN raw_infotrax_orders rio ON ol.order_number = rio.infotrax_order_number
+    LEFT JOIN product_bundle_base pbb ON pbb.sku = ol.item_code
 WHERE ol._FIVETRAN_DELETED = FALSE
 
 
