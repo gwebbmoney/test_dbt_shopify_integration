07e716daf87dcc968076ad3390f5c34f7b69ce7b
Garand Webb <gwebb@red-aspen.com>
2023-10-29 05:59:18 +0000

    alias

--- models/data_marts/redaspen_per_day_discounts.sql
+++ models/data_marts/redaspen_per_day_discounts.sql
@@ -3,7 +3,7 @@ WITH days AS({{dbt_utils.date_spine(
     start_date = "cast('2020-01-01' as date)",
     end_date = 'current_date'
 )}})
-SELECT date_part AS day,
+SELECT d.datepart AS day,
     dpo.distributor_status,
     COALESCE(SUM(dpo.total_discount_amount_cents), 0) AS total_discount_amount_cents
 FROM days d LEFT JOIN {{ ref("redaspen_discount_and_promotion_orders") }} dpo ON d.date_part = dpo.created_at::date
