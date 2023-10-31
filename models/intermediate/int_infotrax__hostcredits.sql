cfe7dcf0fcf9651003ac3827cf4da7dc94b8aaae
Garand Webb <gwebb@red-aspen.com>
2023-10-31 15:18:57 +0000

    BA ID

--- models/intermediate/int_infotrax__hostcredits.sql
+++ models/intermediate/int_infotrax__hostcredits.sql
@@ -8,6 +8,6 @@ order_lines AS(
 SELECT ol.infotrax_order_number AS order_id,
     (ol.retail_amount_cents * -1) AS hostcredit_amount_cents,
     po.created_at,
-    po.brand_ambassador_id,
+    po.brandambassadorid,
     po.distributor_status
 FROM order_lines ol JOIN processed_orders po ON ol.infotrax_order_number = po.infotrax_order_number
