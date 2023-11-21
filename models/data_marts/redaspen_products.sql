86a0f8077af4787c02431ef2f6ce53ccb13a4b61
Garand Webb <gwebb@red-aspen.com>
2023-11-21 18:44:25 +0000

    Forgot the letter "s" for CTE

--- models/data_marts/redaspen_products.sql
+++ models/data_marts/redaspen_products.sql
@@ -15,7 +15,7 @@ WITH products AS(
     FROM {{ ref("int_shopify__products") }} p RIGHT JOIN {{ ref("int_infotrax__products") }} ip ON p.emma_product_id = ip.product_id
 )
 SELECT p.*
-FROM product p LEFT JOIN {{ ref("redaspen_bundles") }} b ON p.shopify_product_id = b.shopify_bundle_id
+FROM products p LEFT JOIN {{ ref("redaspen_bundles") }} b ON p.shopify_product_id = b.shopify_bundle_id
 WHERE b.shopify_bundle_id IS NULL
     AND p.skuable_type = 'Product'
 
