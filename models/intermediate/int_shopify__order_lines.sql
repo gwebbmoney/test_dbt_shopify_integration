6355ed28cd770f500f920dd65384ef6b058884ce
Garand Webb <gwebb@red-aspen.com>
2023-11-29 20:50:04 +0000

    order lines fix

--- models/intermediate/int_shopify__order_lines.sql
+++ models/intermediate/int_shopify__order_lines.sql
@@ -8,7 +8,7 @@ SELECT id AS order_line_id,
         WHEN p.shopify_product_id = ol.product_id THEN p.emma_product_id
         WHEN b.shopify_bundle_id = ol.product_id THEN b.emma_bundle_id
     END) AS emma_product_id,
-    variant_id, 
+    product_variant_id, 
     title AS product_name,
     variant_title AS product_variant_name,
     ol.sku,
@@ -16,8 +16,9 @@ SELECT id AS order_line_id,
     index AS order_line,
     (CASE
         WHEN p.shopify_product_id = ol.product_id THEN 'Product'
-        WHEN b.shopify_bundle_id = ol.product_id THEN b.bundle_type
+        WHEN b.shopify_bundle_id = ol.product_id THEN 'Bundle'
     END) AS skuable_type,
+    bundle_type,
     price*100 AS price_cents,
     quantity AS quantity_ordered,
     fulfillable_quantity,
@@ -25,5 +26,5 @@ SELECT id AS order_line_id,
     total_discount*100 AS total_discount_cents,
     pre_tax_price*100 AS pre_tax_price_cents,
     gift_card
-FROM order_lines ol LEFT JOIN {{ ref("redaspen_product_variants") }} p ON ol.sku = p.sku
-    LEFT JOIN {{ ref("redaspen_bundle_variants") }} b ON ol.sku = b.sku
+FROM order_lines ol LEFT JOIN {{ ref("redaspen_product_variants") }} p ON ol.sku = p.sku AND ol.product_variant_id = p.shopify_product_variant_id
+    LEFT JOIN {{ ref("redaspen_bundle_variants") }} b ON ol.sku = b.sku AND ol.product_variant_id = p.shopify_bundle_variant_id
