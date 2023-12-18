63d56fc76c4ed7b922797571ae75fea700b67e2f
Garand Webb <gwebb@red-aspen.com>
2023-12-18 21:02:31 +0000

    delete refund dimension tables

--- models/data_marts/redaspen_per_day_rmas.sql
+++
@@ -1,8 +0,0 @@
-WITH orders AS(
-    SELECT * FROM {{ ref("int_redaspen__refunds") }}
-)
-SELECT refunded_at::date AS day,
-    SUM(refund_amount_cents) AS total_rmas_cents
-FROM orders
-GROUP BY day 
-
