103afb821e40fabadf55a1ef7ed0bfe5832d8f50
Garand Webb <gwebb@red-aspen.com>
2023-12-20 18:23:32 +0000

    delete dimensional tables

--- models/data_marts/redaspen_per_day_sales_tax_and_shipping_revenues.sql
+++
@@ -1,9 +0,0 @@
-WITH per_day_sales_tax_and_shipping_revenue AS(
-    SELECT created_at::date AS day,
-        SUM(sales_tax_amount_cents - sales_tax_refund_cents) AS sales_tax_cents,
-        SUM(shipping_amount_cents - shipping_refund_cents) AS shipping_amount_cents
-    FROM {{ ref("redaspen_processed_orders") }}
-    GROUP BY day
-)
-SELECT *
-FROM per_day_sales_tax_and_shipping_revenue
--- models/data_marts/redaspen_per_month_discounts.sql
+++
@@ -1,7 +0,0 @@
-SELECT MONTH(day) AS month,
-    YEAR(day) AS YEAR,
-    distributor_status,
-    COALESCE(SUM(total_discount_amount_cents), 0) AS total_discount_amount_cents
-FROM {{ ref("redaspen_per_day_discounts") }}
-GROUP BY month, year, distributor_status
-ORDER BY month, year
