309c7cd05404bd135e5ebb7f6a66e197f654701d
Garand Webb <gwebb@red-aspen.com>
2024-01-23 22:54:58 +0000

    note different naming conventions for redaspen_v2 to redaspen switch

--- models/data_marts/redaspen_hostcredits.sql
+++ models/data_marts/redaspen_hostcredits.sql
@@ -1,3 +1,8 @@
+--{{ config(database = 'redaspen') }}
+--Will be used to transfer this table to the REDASPEN Schema
+
+--{{config(schema = 'transaction_metrics') }}
+
 {{dbt_utils.union_relations(
     relations = [ref('int_shopify__giftcards'), ref('int_infotrax__hostcredits')]
 )}}
