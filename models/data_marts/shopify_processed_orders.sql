4214b569b6d8d5fdf9782b5434d9d5428bf81519
gwebb@red-aspen.com <gwebb@red-aspen.com>
2024-03-20 22:14:51 +0000

    New commit for processed orders

--- models/data_marts/shopify_processed_orders.sql
+++ models/data_marts/shopify_processed_orders.sql
@@ -7,8 +7,7 @@ WITH orders AS(
 )
 SELECT *
 FROM orders
-WHERE financial_status IN ('paid', 'partially_refunded')
-    AND (fulfillment_status <> 'cancelled' OR fulfillment_status IS NULL)
+WHERE (fulfillment_status NOT IN ('cancelled', 'unfulfilled') OR fulfillment_status IS NULL)
 --Will probably have to change what qualifies for a processed order later
 --For now, keep fulfillment_status as 'fulfilled'
 --Questions: Is an item fulfilled if the order is completely refunded/partially refunded
