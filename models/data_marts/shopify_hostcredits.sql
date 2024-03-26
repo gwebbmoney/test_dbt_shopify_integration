5b377f67fbe9c53079c749ab1377616b8d47b877
gwebb@red-aspen.com <gwebb@red-aspen.com>
2024-03-26 15:43:50 +0000

    edits made to hostcredit amounts

--- models/data_marts/shopify_hostcredits.sql
+++ models/data_marts/shopify_hostcredits.sql
@@ -2,6 +2,30 @@
 
 {{ config(schema = 'transaction_metrics')}}
 
-{{dbt_utils.union_relations(
-    relations = [ref('int_shopify__giftcards'), ref('int_infotrax__hostcredits')]
-)}}
+WITH infotrax_hostcredits AS(
+        SELECT *
+        FROM {{ ref("int_infotrax__hostcredits") }}
+),
+shopify_pop_ups AS(
+        SELECT dpo.order_id,
+            (dpo.total_discount_amount_cents * -1) AS hostcredit_amount_cents,
+            dpo.created_at,
+            o.brand_ambassador_id,
+            dpo.distributor_status
+        FROM {{ ref("shopify_discount_and_promotion_orders") }} dpo JOIN {{ ref("shopify_orders") }} o ON dpo.order_id = o.order_id
+        WHERE order_discount_code LIKE '%POP%'
+            AND order_discount_code LIKE '%TS%'
+)
+SELECT order_id,
+    hostcredit_amount_cents,
+    created_at,
+    brand_ambassador_id,
+    distributor_status
+FROM shopify_pop_ups
+UNION
+SELECT order_id,
+    hostcredit_amount_cents,
+    created_at,
+    brand_ambassador_id,
+    distributor_status
+FROM infotrax_hostcredits
