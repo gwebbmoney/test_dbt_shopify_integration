c9c43ab437ca7331b1f304e77d9db044bc1a5206
Oscar Zelaya Vigil <oscar.zelaya@brooklyndata.co>
2024-08-21 10:36:15 -0600

    modularization of intermediate product models

--- models/intermediate/int_infotrax__products.sql
+++ models/intermediate/int_infotrax__products.sql
@@ -1,66 +1,114 @@
 -- Creates view of all products and bundles shown within EMMA
 -- NOTE: EMMA is our in-house application that houses various company data. For this purpose, we grab product information from this resource and attach it to the order lines table
-SELECT p.id AS product_id,
-    s.name AS sku,
-    p.name AS product_title,
-    c.id AS category_id,
-    c.name AS product_type,
-    sc.id AS sub_category_id,
-    sc.name AS sub_category_name,
-    p.seasonality,
-    p.collection,
-    p.style_id,
-    p.style,
-    p.length_id,
-    p.length,
-    p.shape_id,
-    p.shape,
-    p.design,
-    p.volume_id,
-    p.volume,
-    p.status_id,
-    p.status,
-    p.finish,
-    p.price,
-    p.pv,
-    p.component,
-    NULL AS value,
-    s.skuable_type
-FROM {{ source("normalized_snowflake", 'PRODUCTS') }} p LEFT JOIN {{ source("redaspen", 'SKUS') }} s ON p.id = s.skuable_id
-    LEFT JOIN {{ source("redaspen", 'CATEGORIES') }} c ON p.category_id = c.id
-    LEFT JOIN {{ source("redaspen", 'SUB_CATEGORIES') }} sc ON p.sub_category_id = sc.id
-WHERE s.skuable_type = 'Product'
--- Grabs all products within EMMA
-UNION
-SELECT b.id AS bundle_id,
-    s.name AS sku,
-    b.name AS bundle_title,
-    NULL AS category_id,
-    NULL AS product_type,
-    NULL AS sub_category_id,
-    NULL AS sub_category_name,
-    NULL AS seasonality,
-    NULL AS collection,
-    NULL AS style_id,
-    NULL AS style,
-    NULL AS length_id,
-    NULL AS length,
-    NULL AS shape_id,
-    NULL AS shape,
-    NULL AS design,
-    NULL AS volume_id,
-    NULL AS volume,
-    b.status_id AS status_id,
-    NULL AS status,
-    NULL AS finish,
-    b.price AS price,
-    b.pv AS pv,
-    NULL AS component,
-    b.value AS value,
-    s.skuable_type
-FROM {{ source("redaspen", 'BUNDLES') }} b LEFT JOIN {{ source("redaspen", 'SKUS') }} s ON b.id = s.skuable_id
-WHERE s.skuable_type = 'Bundle'
--- Grabs all bundles within EMMA
+with
+    normalized_products as (
+        select *
+        from {{ ref('source_normalized_snowflake__products') }}
+    ),
+    skus as (
+        select *
+        from {{ ref('source_emma__skus') }}
+    ),
+    categories as (
+        select *
+        from {{ ref('source_emma__categories') }}
+    ),
+    sub_categories as (
+        select *
+        from {{ ref('source_emma__sub_categories')}}
+    ),
+    bundles as (
+        select *
+        from {{ ref('source_emma__bundles')}}
+    ),
 
+    emma_products as (
+        -- Grabs all products within EMMA
+        select
+            {{ dbt_utils.generate_surrogate_key([
+                'normalized_products.id', 
+                'skus.sku'
+            ]) }} as id,
+            normalized_products.id as product_id,
+            skus.sku,
+            normalized_products.product_title,
+            categories.id as category_id,
+            categories.product_type,
+            sub_categories.id as sub_category_id,
+            sub_categories.sub_category_name,
+            normalized_products.seasonality,
+            normalized_products.collection,
+            normalized_products.style_id,
+            normalized_products.style,
+            normalized_products.length_id,
+            normalized_products.length,
+            normalized_products.shape_id,
+            normalized_products.shape,
+            normalized_products.design,
+            normalized_products.volume_id,
+            normalized_products.volume,
+            normalized_products.status_id,
+            normalized_products.status,
+            normalized_products.finish,
+            normalized_products.price,
+            normalized_products.pv,
+            normalized_products.is_component,
+            null as value,
+            skus.skuable_type
+        from normalized_products
+        left join skus
+            on normalized_products.id = skus.skuable_id
+        left join categories 
+            on normalized_products.category_id = categories.id
+        left join sub_categories
+            on normalized_products.sub_category_id = sub_categories.id
+        where trim(lower(skus.skuable_type)) = 'product'
+    ),
 
+    emma_bundles as (
+    -- Grabs all bundles within EMMA
+        select
+            {{ dbt_utils.generate_surrogate_key([
+                'bundles.id', 
+                'skus.sku'
+            ]) }} as id,
+            bundles.id as bundle_id,
+            skus.sku,
+            bundles.bundle_title,
+            null as category_id,
+            null as product_type,
+            null as sub_category_id,
+            null as sub_category_name,
+            null as seasonality,
+            null as collection,
+            null as style_id,
+            null as style,
+            null as length_id,
+            null as length,
+            null as shape_id,
+            null as shape,
+            null as design,
+            null as volume_id,
+            null as volume,
+            bundles.status_id,
+            null as status,
+            null as finish,
+            bundles.price,
+            bundles.pv,
+            null as component,
+            bundles.value,
+            skus.skuable_type
+        from bundles
+        left join skus on bundles.id = skus.skuable_id
+        where trim(lower(skus.skuable_type)) = 'bundle'
+    ),
 
+    unioned as (
+        select *
+        from emma_products
+        union all
+        select *
+        from emma_bundles
+    )
+
+select * from unioned
--- models/intermediate/int_shopify__products.sql
+++ models/intermediate/int_shopify__products.sql
@@ -1,45 +1,63 @@
 -- Creates a view that grabs all products that are within our Shopify system
 -- NOTE: EMMA is our in-house application that houses various company data. For this purpose, we grab product information from this resource and attach it to the order lines table
-WITH products AS(
-    SELECT id AS product_id,
-        title AS product_title,
-        product_type,
-        status,
-        created_at,
-        updated_at,
-        published_at
-    FROM {{ source('shopify_raw', 'PRODUCT') }}
-    WHERE _fivetran_deleted = FALSE
--- Grabs Shopify products in the SHOPIFY.PRODUCT table
-),
-product_metafield_emma_id AS(
-    SELECT owner_id,
-        key,
-        value
-    FROM {{ source('shopify_raw', 'METAFIELD') }}
-    WHERE owner_resource IN ('product', 'PRODUCT')
-        AND key = 'emma_id'
--- Grabs the EMMA product id for each product
-),
-product_metafield_sub_category AS(
-    SELECT owner_id,
-        key,
-        value
-    FROM {{ source('shopify_raw', 'METAFIELD') }}
-    WHERE owner_resource IN ('product', 'PRODUCT')
-        AND key = 'product_sub_category'
--- Grabs the EMMA sub category id for each product
-)
-SELECT product_id,
-    product_title,
-    product_type,
-    status,
-    created_at,
-    updated_at,
-    published_at,
-    (CASE WHEN pm.key = 'emma_id' THEN pm.value END) AS emma_product_id,
-    (CASE WHEN pmsc.key = 'product_sub_category' THEN pmsc.value END) AS sub_category_name
-FROM products p LEFT JOIN product_metafield_emma_id pm ON p.product_id = pm.owner_id
-    LEFT JOIN product_metafield_sub_category pmsc ON p.product_id = pmsc.owner_id
--- Organizes product information from Shopify
+with
+    products as (
+        -- Grabs Shopify products in the SHOPIFY.PRODUCT table
+        select 
+            id as product_id,
+            product_title,
+            product_type,
+            status,
+            created_at,
+            updated_at,
+            published_at
+        from {{ ref('source_shopify__products') }}
+    ),
+    
+    metafield as (
+        select 
+            owner_id,
+            key,
+            value
+        from {{ ref('source_shopify__metafield') }}
+        where trim(lower(owner_resource)) = 'product' 
+    ),
 
+    product_metafield_emma_id as (
+        -- Grabs the EMMA product id for each product
+        select *
+        from metafield
+        where key = 'emma_id'
+    ),
+
+    product_metafield_sub_category as (
+        -- Grabs the EMMA sub category id for each product
+        select *
+        from metafield
+        where key = 'product_sub_category'
+    )
+
+-- Organizes product information from Shopify
+select
+    /* Primary key */
+    products.product_id,
+
+    /* Foreign keys and IDs */
+    product_emma_id.value as emma_product_id,
+
+    /* Timestamps */
+    products.created_at,
+    products.updated_at,
+    products.published_at,
+
+    /* Status and properties */
+    product_sub_category.value as sub_category_name,
+    products.product_title,
+    products.product_type,
+    products.status
+
+from products 
+left join product_metafield_emma_id as product_emma_id 
+    on products.product_id = product_emma_id.owner_id
+left join product_metafield_sub_category as product_sub_category  
+    on products.product_id = product_sub_category.owner_id
